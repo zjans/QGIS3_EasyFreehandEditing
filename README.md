b47765f2e784568772208eda247c6e23efeb3883
Jan Schindler <39352402+zjans@users.noreply.github.com>
2021-08-19 18:43:23 +1200

    simplified click and drag behaviour

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# freehandEditing_3
-FreehandEditing_3 is an update from earlier freehandEditing Tool to QGIS 3.x. With this tool the user is able to create geometries with the help of mouse track.
+# EasyFreehandEditing
+*EasyFreehandEditing* is an update from *FreehandEditing_3*, which itself is an update from earlier freehandEditing Tool to QGIS 3.x. With this tool the user is able to create geometries with the help of mouse track.
 ### Manual:
 1. choose a line- or polygonlayer
 2. switch to edit mode
--- freehandeditingtool.py
+++ freehandeditingtool.py
@@ -51,78 +51,75 @@ class FreehandEditingTool(QgsMapTool):
             print("ctrl release")
 
     def canvasPressEvent(self, event):
+        if not self.rb:
+            #  and self.mCtrl
+            if self.ignoreclick or self.drawing:
+                return
+            layer = self.canvas.currentLayer()
+            if layer is None:
+                return
+            self.drawing = True
+            self.type = layer.geometryType()
+            self.isPolygon = (self.type != 1)
+            if self.isPolygon:
+                self.rb = QgsRubberBand(self.canvas, QgsWkbTypes.PolygonGeometry)
+                self.rb.setColor(QColor(255, 0, 0, 63))
+                self.rb.setWidth(2)
+            else:
+                self.rb = QgsRubberBand(self.canvas)
+                self.rb.setColor(QColor(255, 0, 0, 150))
+                self.rb.setWidth(1)
+            x = event.pos().x()
+            y = event.pos().y()
+            if self.type == 1:
+                if self.mCtrl:
 
-        if self.ignoreclick or self.drawing:
+                    startingPoint = QPoint(x, y)
+                    snapper = QgsMapCanvasSnappingUtils(self.canvas)
+                    result = snapper.snapToCurrentLayer(startingPoint,QgsPointLocator.Vertex)
 
-            # ignore secondary canvasPressEvents if already drag-drawing
-            # NOTE: canvasReleaseEvent will still occur (ensures rb is deleted)
-            # click on multi-button input device will halt drag-drawing
-            return
-        layer = self.canvas.currentLayer()
-        if layer is None:
-            return
-        self.drawing = True
-        self.type = layer.geometryType()
-        self.isPolygon = (self.type != 1)
-        if self.isPolygon:
+                    if result.point().x() != 0 and result.point().y() != 0:
 
-            self.rb = QgsRubberBand(self.canvas, QgsWkbTypes.PolygonGeometry)
-            self.rb.setColor(QColor(255, 0, 0, 63))
-            self.rb.setWidth(2)
-        else:
+                        point = QgsPointXY()
+                        point.setX(result.point().x())
+                        point.setY(result.point().y())
 
-            self.rb = QgsRubberBand(self.canvas)
-            self.rb.setColor(QColor(255, 0, 0, 150))
-            self.rb.setWidth(1)
-        x = event.pos().x()
-        y = event.pos().y()
-        if self.type == 1:
-            if self.mCtrl:
+                    else:
 
-                startingPoint = QPoint(x, y)
-                snapper = QgsMapCanvasSnappingUtils(self.canvas)
-                result = snapper.snapToCurrentLayer(startingPoint,QgsPointLocator.Vertex)
-
-                if result.point().x() != 0 and result.point().y() != 0:
-
-                    point = QgsPointXY()
-                    point.setX(result.point().x())
-                    point.setY(result.point().y())
-
+                        point = self.toLayerCoordinates(layer, event.pos())
                 else:
-
                     point = self.toLayerCoordinates(layer, event.pos())
+
+                pointMap = self.toMapCoordinates(layer, point)
+                self.rb.addPoint(pointMap)
             else:
                 point = self.toLayerCoordinates(layer, event.pos())
+                pointMap = self.toMapCoordinates(layer, point)
+                self.rb.addPoint(pointMap)
 
-            pointMap = self.toMapCoordinates(layer, point)
-            self.rb.addPoint(pointMap)
         else:
-            point = self.toLayerCoordinates(layer, event.pos())
-            pointMap = self.toMapCoordinates(layer, point)
-            self.rb.addPoint(pointMap)
+            # if not self.mCtrl:
+            #     return
+            if self.ignoreclick:
+                return
+            if not self.rb:
+                return
+            self.drawing = False
+            if self.rb.numberOfVertices() > 2:
+                geom = self.rb.asGeometry()
+                self.rbFinished.emit(geom)
+
+            # reset rubberband and refresh the canvas
+            self.rb.reset()
+            self.rb = None
+            self.isPolygon = (self.type != 1)
+            self.canvas.refresh()
 
     def canvasMoveEvent(self, event):
-        if self.ignoreclick or not self.rb:
-            return
-        self.rb.addPoint(self.toMapCoordinates(event.pos()))
-
-
-    def canvasReleaseEvent(self, event):
         if self.ignoreclick:
             return
-        self.drawing = False
-        if not self.rb:
-            return
-        if self.rb.numberOfVertices() > 2:
-            geom = self.rb.asGeometry()
-            self.rbFinished.emit(geom)
-
-        # reset rubberband and refresh the canvas
-        self.rb.reset()
-        self.rb = None
-        self.isPolygon = (self.type != 1)
-        self.canvas.refresh()
+        if self.rb:
+            self.rb.addPoint(self.toMapCoordinates(event.pos()))
 
     def setIgnoreClick(self, ignore):
         """Used to keep the tool from registering clicks during modal dialogs"""
